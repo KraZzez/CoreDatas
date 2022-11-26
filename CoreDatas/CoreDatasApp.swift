3a8e197002193262a84e39b412dc8a4dde984d02
KraZzez <ludvig.ludde23@gmail.com>
2022-11-26 15:17:57 +0100

    Initial Commit

---
+++ CoreDatas/CoreDatasApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDatasApp.swift
+//  CoreDatas
+//
+//  Created by Ludvig Krantzén on 2022-11-26.
+//
+
+import SwiftUI
+
+@main
+struct CoreDatasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
